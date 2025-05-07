b9997d1991ec117f8f1ba970da67fef4942229a7
wwelden <wwelden@trinity.edu>
2025-05-07 00:08:04 -0500

    update

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -1,6 +1,23 @@
 using UnityEngine;
 
-public class Ghost : Entity {
+public class Ghost : Entity
+{
+    public Vector3 Position;
+    public float speed = 1;
+    public int range = 5;
+    public bool BlueMode = false;
+
+    public void Update()
+    {
+
+    }
+
+    public void checkRange()
+    {
+        // look for pacman in range. 
+
+    }
+
 
 
 
